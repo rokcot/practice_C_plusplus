786c900dba8b2cf065025ef58f566370005c0f65
Nikolay Grigoryev <nikolay@cppqt.dev>
2020-02-14 12:28:08 +0300

    Improved sorting. In length and alphabet.

--- hello_array.cpp
+++ hello_array.cpp
@@ -3,40 +3,37 @@
 
 using namespace std;
 
-int calculateMax(string * array)
+int calculateMin(string array[], bool is_min)
 {
-	return 0;
-}
-
-int calculateMin(string * array,bool min_max)
-{
-	int len_min = array[0].size(), len_max = array[0].size();
-	string min_lenS = array[0];
-	string max_lenS = array[0];
+	int result = array[0].size();
 	for (int i = 0; i < 20; ++i)
 	{
-		if (len_min > array[i].size())
+		if (is_min)
 		{
-			len_min = array[i].size();
-			min_lenS = array[i];
+			if (result > array[i].size())
+			{
+				result = array[i].size();
+			}
 		}
-		if (len_max < array[i].size())
+		else
 		{
-			len_max = array[i].size();
-			max_lenS = array[i];
+			if (result < array[i].size())
+			{
+				result = array[i].size();
+			}
 		}
 	}
-	if(min_max == true)
-	return len_min;
-	else return len_max;
+	return result;
 }
+
+
 
 int main()
 {
 	const int N = 20; //Длина массива
-
 	setlocale(0, "");
 	int len_min = 0, len_max = 0;
+	string temp;
 	string town[] = { "Miami","Houston","Atlanta","Boston","New York","Dayton","Chicago","Seattle","Mesa","Tucson","Dallas","Amarillo",
 					"Bozeman","Butte","Casper","Linciln","Onaha","Fargo","Peoria","Toledo","Detroit","London","Hamilton","Ottawa" };
 
@@ -50,7 +47,7 @@ int main()
 	cout << "===========================" << endl;
 	for (int i = 0; i < N; ++i)
 	{
-		if (len_min >= town[i].size())
+		if (len_min == town[i].size())
 		{
 			len_min = town[i].size();
 			min_lenS = town[i];
@@ -63,7 +60,7 @@ int main()
 	cout << "===========================" << endl;
 	for (int i = 0; i < N; ++i)
 	{
-		if (len_max <= town[i].size())
+		if (len_max == town[i].size())
 		{
 			len_max = town[i].size();
 			max_lenS = town[i];
@@ -72,24 +69,21 @@ int main()
 	}
 	cout << "===========================" << endl;
 
-	cout << "Сортировка по длине:" << endl;
+	cout << "Сортировка по длине и алфавиту:" << endl;
 	cout << "===========================" << endl;
-	for (int i = 0; i < N; ++i)
-	{
-		if (len_min < town[i].size())
-		{
-			len_min = town[i].size();
-			min_lenS = town[i];
-
-			for (int j = 0; j < N; ++j) {
-				if (len_min == town[j].size())
-				{
-					len_min = town[j].size();
-					min_lenS = town[j];
-					cout << " " << min_lenS << endl;
-				}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - i; j++) {
+			if (town[j].size() > town[j + 1].size() || (town[j].size() == town[j + 1].size() && town[j] > town[j + 1])) {
+				// меняем элементы местами
+				temp = town[j];
+				town[j] = town[j + 1];
+				town[j + 1] = temp;
 			}
 		}
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		cout << town[i] << endl;
 	}
 	cout << "===========================" << endl;
 	system("pause");
